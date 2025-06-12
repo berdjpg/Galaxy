a1cac9ee38fa48c470f22aad7fe8907f8dfb10c0
Berend <hi@berendvandruten.nl>
2025-06-12 21:44:26 +0200

    .

--- pages/api/alogs.js
+++ pages/api/alogs.js
@@ -1,7 +1,4 @@
-// /api/fetch-adventure-logs.ts
-
 import { createClient } from '@supabase/supabase-js';
-import iconv from 'iconv-lite';
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
